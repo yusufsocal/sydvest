8b94023d3a12c6a2418421890ab03e052769c40f
Peder HN <pederhn@uio.no>
2026-04-28 10:11:50 +0200

    Fikset søkebar

--- app/src/main/java/no/uio/ifi/in2000/dylansc/team6project/ui/map/components/MapSearchField.kt
+++ app/src/main/java/no/uio/ifi/in2000/dylansc/team6project/ui/map/components/MapSearchField.kt
@@ -1,32 +1,39 @@
 package no.uio.ifi.in2000.dylansc.team6project.ui.map.components
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MenuAnchorType
-import androidx.compose.material3.ExposedDropdownMenuBox
-import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.ListItem
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.SearchBar
+import androidx.compose.material3.SearchBarDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
-import androidx.compose.ui.Alignment
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Color.Companion.White
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.zIndex
 import no.uio.ifi.in2000.dylansc.team6project.data.searchdata.SearchResult
 import androidx.compose.ui.graphics.Color as ComposeColor
 
@@ -36,64 +43,134 @@ fun MapSearchField(
     suggestions: List<SearchResult>,
     onQueryChange: (String) -> Unit,
     onSuggestionSelected: (SearchResult) -> Unit,
-    onDismiss: () -> Unit,
 ) {
     var query by remember { mutableStateOf("") }
-    Box(modifier = Modifier.fillMaxSize()) {
-        Column(
-            verticalArrangement = Arrangement.spacedBy(16.dp, alignment = Alignment.Bottom),
-            modifier = Modifier
-                .align(Alignment.TopCenter)
-                .padding(35.dp)
-        ) {
-            ExposedDropdownMenuBox(
-                expanded = suggestions.isNotEmpty(),
-                onExpandedChange = {},
-                modifier = Modifier
-                    .height(50.dp)
-                    .fillMaxWidth()
-                    .background(White, shape = RoundedCornerShape(25.dp))
-                    .border(
-                        width = 1.dp,
-                        color = ComposeColor.Black,
-                        shape = RoundedCornerShape(25.dp)
-                    )
-                    .padding(16.dp)
-            ) {
-                TextField(
-                    value = query,
-                    onValueChange = { newText ->
-                        query = newText
-                        onQueryChange(newText)
-                    },
-                    label = { Text("Stedsnavn") },
-                    colors = ExposedDropdownMenuDefaults.textFieldColors(
-                        focusedContainerColor = ComposeColor(0xFFF7FCFE),
-                        unfocusedContainerColor = ComposeColor(0xFFF7FCFE)
-                    ),
-                    modifier = Modifier
-                        .menuAnchor(MenuAnchorType.PrimaryEditable)
-                        .fillMaxWidth()
-                )
+    var isFocused by remember { mutableStateOf(false) }
+    var expanded by remember { mutableStateOf(false) }
 
-                ExposedDropdownMenu(
-                    expanded = suggestions.isNotEmpty(),
-                    onDismissRequest = onDismiss
-                ) {
-                    suggestions.forEach { suggestion ->
-                        DropdownMenuItem(
-                            text = { Text(suggestion.name) },
-                            onClick = {
-                                query = suggestion.name
-                                onSuggestionSelected(suggestion)
+
+    Box(
+        modifier = Modifier
+            .then(if (isFocused) Modifier.fillMaxSize() else Modifier.fillMaxWidth())
+            .zIndex(2f)
+    ) {
+        if (expanded) {
+            //Bakgrunn bak søkefeltet
+            //Opprettes kun når man søker etter noe
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(MaterialTheme.colorScheme.surface)
+                    .clickable { expanded = false }
+            )
+        }
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(25.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            //Selve søkefelt
+            SearchBar(
+                inputField = {
+                    TextField(
+                        value = query,
+                        onValueChange = {
+                            query = it
+                            onQueryChange(it)
+                            if (suggestions.isNotEmpty() && !expanded) {
+                                expanded = true
                             }
+                        },
+                        placeholder = { Text("Søk", color = MaterialTheme.colorScheme.onSurface) },
+                        leadingIcon = {
+                            Icon(
+                                imageVector = Icons.Default.Search,
+                                contentDescription = "Search Icon"
+                            )
+                        },
+                        trailingIcon = {
+                            if (query.isNotEmpty()) {
+                                IconButton(
+                                    onClick = {
+                                        query = ""
+                                        expanded = false
+                                    }
+                                ) {
+                                    Icon(
+                                        imageVector = Icons.Default.Clear,
+                                        contentDescription = "Clear search"
+                                    )
+                                }
+                            }
+                        },
+                        singleLine = true,
+                        //Farger for søkefeltet
+                        colors = TextFieldDefaults.colors(
+                            focusedContainerColor = ComposeColor.Transparent,
+                            unfocusedContainerColor = ComposeColor.Transparent,
+                            disabledContainerColor = ComposeColor.Transparent,
+                            focusedIndicatorColor = ComposeColor.Transparent,
+                            unfocusedIndicatorColor = ComposeColor.Transparent,
+                        ),
+
                         )
+                },
+                expanded = expanded,
+                onExpandedChange = { expanded = it },
+                shape =  RoundedCornerShape(16f.dp),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .onFocusChanged { focusState ->
+                        isFocused = !isFocused},
+                content = {
+                    LazyColumn(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .background(MaterialTheme.colorScheme.surface)
+                    ) {
+
+                        if (suggestions.isEmpty() && query.isNotEmpty()) {
+                            item {
+                                ListItem(
+                                    headlineContent = {
+                                        Text(
+                                            "Fant ingen resultater for \"$query\"\n¯\\_(ツ)_/¯",
+                                            color = MaterialTheme.colorScheme.onSurface
+                                        )
+                                    }
+                                )
+                            }
+                        } else {
+                            items(suggestions) { suggestion ->
+                                ListItem(
+                                    headlineContent = {
+                                        Text(
+                                            suggestion.name,
+                                            color = MaterialTheme.colorScheme.onSurface,
+                                        )
+                                    },
+                                    leadingContent = {
+                                        Icon(
+                                            Icons.Default.Search,
+                                            contentDescription = null
+                                        )
+                                    },
+                                    modifier = Modifier.clickable {
+                                        query = suggestion.name
+                                        expanded = false
+                                        onSuggestionSelected(suggestion) // Sender resultatet tilbake
+                                    }
+
+                                )
+                            }
+                        }
                     }
                 }
-            }
+            )
         }
-
-
     }
 
 }
+
+
