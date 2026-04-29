6ceb02c245d0dedba120853e9355eaa35439af7d
Peder HN <pederhn@uio.no>
2026-04-29 13:55:35 +0200

    fikset timeslider

--- app/src/main/java/no/uio/ifi/in2000/dylansc/team6project/ui/map/components/MapTimeSliderSection.kt
+++ app/src/main/java/no/uio/ifi/in2000/dylansc/team6project/ui/map/components/MapTimeSliderSection.kt
@@ -1,12 +1,8 @@
 package no.uio.ifi.in2000.dylansc.team6project.ui.map.components
 
-import android.R.attr.font
-import android.R.attr.text
 import android.os.Build
 import androidx.annotation.RequiresApi
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
@@ -14,25 +10,24 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Slider
 import androidx.compose.material3.SliderDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.zIndex
 import no.uio.ifi.in2000.dylansc.team6project.R
 import no.uio.ifi.in2000.dylansc.team6project.data.weatherdata.WMSLayer
 import no.uio.ifi.in2000.dylansc.team6project.ui.map.MapViewModel
-import androidx.compose.ui.graphics.Color as ComposeColor
 
 @RequiresApi(Build.VERSION_CODES.O)
 @OptIn(ExperimentalMaterial3Api::class)
@@ -47,7 +42,7 @@ fun MapTimeSliderSection(
 
     selectedLayer: WMSLayer?
 
-    ) {
+) {
 
     Row(
         horizontalArrangement = Arrangement.SpaceBetween,
@@ -62,26 +57,34 @@ fun MapTimeSliderSection(
         Text(text = "+${sliderPosition.toInt().toString()}T")
     }
     if (selectedLayer != null) {
-        Slider(
-            value = sliderPosition,
-            onValueChange = onSliderChange,
-            colors = SliderDefaults.colors(
-                activeTrackColor = MaterialTheme.colorScheme.secondary,
-                inactiveTrackColor = MaterialTheme.colorScheme.secondaryContainer,
-            ),
-            thumb = {
-                Image (
+        Row(){
+            Button(
+                onClick = { onAnimateToggle() },
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.Transparent
+                ),
+                modifier = Modifier.clip(CircleShape)
+            ) {
+                Image(
                     painter = if (isAnimating) painterResource(id = R.drawable.pause) else painterResource(id = R.drawable.play),
                     contentDescription = null,
                     modifier = Modifier
                         .size(30.dp)
                         .clip(CircleShape)
-                        .clickable {onAnimateToggle()}
                 )
-            },
-            steps = 239,
-            valueRange = 0f..240f,
-        )
+            }
+            Slider(
+                value = sliderPosition,
+                onValueChange = onSliderChange,
+                colors = SliderDefaults.colors(
+                    activeTrackColor = MaterialTheme.colorScheme.secondary,
+                    inactiveTrackColor = MaterialTheme.colorScheme.secondaryContainer,
+                ),
+                steps = 239,
+                valueRange = 0f..240f,
+            )
+        }
+
     }
 
 }
