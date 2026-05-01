6027a8d241a9e8314e23ada183321deeb88462ea
Silje Becmann Granås <siljbg@ulrik.uio.no>
2026-05-01 09:50:01 +0200

    Lagt til utvidet ikon-bibliotek i gradle

--- app/src/main/java/no/uio/ifi/in2000/dylansc/team6project/data/weatherdata/AreaData.kt
+++ app/src/main/java/no/uio/ifi/in2000/dylansc/team6project/data/weatherdata/AreaData.kt
@@ -1,8 +1,8 @@
 package no.uio.ifi.in2000.dylansc.team6project.data.weatherdata
 
 enum class AreaData(val area: String) {
-    NORDEN("meps_det_vdiv_2_5km_calculations"),
-    ARKTIS("arome_arctic_det_vdiv_2_5km_calculations"),
-    VERDEN("ec_sfc_3h_calculations")
+    NORDIC("meps_det_vdiv_2_5km_calculations"),
+    ARCTIC("arome_arctic_det_vdiv_2_5km_calculations"),
+    WORLD("ec_sfc_3h_calculations")
 
 }
