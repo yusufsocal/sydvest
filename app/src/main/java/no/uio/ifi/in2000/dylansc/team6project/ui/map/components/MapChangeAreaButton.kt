50bdfdbea888ac94376986fc618ab5a9e7ef5473
Silje Becmann Granås <siljbg@ulrik.uio.no>
2026-05-07 19:09:11 +0200

    lett refaktorering av MapChangeAreaButton.kt

--- app/src/main/java/no/uio/ifi/in2000/dylansc/team6project/ui/map/components/MapChangeAreaButton.kt
+++ app/src/main/java/no/uio/ifi/in2000/dylansc/team6project/ui/map/components/MapChangeAreaButton.kt
@@ -1,44 +1,23 @@
 package no.uio.ifi.in2000.dylansc.team6project.ui.map.components
 
-import android.R.attr.contentDescription
-import android.R.attr.text
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Info
-import androidx.compose.material.icons.filled.Public
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
-import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.ExposedDropdownMenuBox
-import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.MenuAnchorType
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color.Companion.White
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import no.uio.ifi.in2000.dylansc.team6project.data.weatherdata.AreaData
-import org.osmdroid.views.MapView
+import no.uio.ifi.in2000.dylansc.team6project.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -50,9 +29,13 @@ fun MapChangeAreaButton(
     ) {
 
     //Change area from button
-    val areaOptions = listOf("Norden", "Arktis", "Verden")
+    val areaOptions = listOf(
+        stringResource(R.string.nordic),
+        stringResource(R.string.arctic),
+        stringResource(R.string.worldwide))
 
-    Card() {
+
+    Card {
         Column(
             modifier = Modifier.padding(16.dp),
             horizontalAlignment = Alignment.CenterHorizontally,
@@ -61,7 +44,7 @@ fun MapChangeAreaButton(
             IconButton(onClick = { onShowAreaChange() }) {
                 Icon(
                     imageVector = Icons.Default.Info,
-                    contentDescription = "Velg mellom områder",
+                    contentDescription = stringResource(R.string.choose_data_source_area),
                     tint = MaterialTheme.colorScheme.primary
                 )
             }
