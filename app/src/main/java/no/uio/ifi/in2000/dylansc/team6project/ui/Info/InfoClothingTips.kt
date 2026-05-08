1a6f67bcf0a86ba163a64ad004c77edc1cc81ada
Silje Becmann Granås <siljbg@ulrik.uio.no>
2026-05-08 17:21:02 +0200

    lagt til knapp for utdypende info, ikke logikk og endestasjon enda

--- app/src/main/java/no/uio/ifi/in2000/dylansc/team6project/ui/Info/InfoClothingTips.kt
+++ app/src/main/java/no/uio/ifi/in2000/dylansc/team6project/ui/Info/InfoClothingTips.kt
@@ -1,9 +1,17 @@
 package no.uio.ifi.in2000.dylansc.team6project.ui.Info
 
+
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.outlined.Info
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -12,8 +20,17 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.FilledIconButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconButtonDefaults
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import no.uio.ifi.in2000.dylansc.team6project.model.domene.CurrentWeather
 import no.uio.ifi.in2000.dylansc.team6project.model.domene.getClothingTips
+import no.uio.ifi.in2000.dylansc.team6project.R
 
 @Composable
 fun InfoClothingTips (weather: CurrentWeather) {
@@ -27,14 +44,34 @@ fun InfoClothingTips (weather: CurrentWeather) {
         border = BorderStroke(1.dp, MaterialTheme.colorScheme.outlineVariant),
         elevation = CardDefaults.cardElevation(defaultElevation = 0.dp),
         modifier = Modifier.fillMaxWidth()) {
-        Text(
-            text = "Klestips:",
-            style = MaterialTheme.typography.labelLarge,
-            modifier = Modifier.padding(8.dp)
-        )
+
+        Row (
+            modifier = Modifier.fillMaxWidth()
+                .padding(8.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = stringResource(R.string.clothing_advise),
+                fontWeight = FontWeight.Bold,
+                style = MaterialTheme.typography.labelLarge,
+                modifier = Modifier.padding(8.dp)
+            )
+
+            IconButton(
+                onClick = {TODO()}
+            ) { Icon(
+                modifier = Modifier
+                    .size(24.dp),
+                imageVector = Icons.Default.Info,
+                contentDescription = null,
+                tint = MaterialTheme.colorScheme.onSurfaceVariant
+            )
+            }
+        }
 
         Text (tip,
-            modifier = Modifier.padding (8.dp))
+            modifier = Modifier.padding (16.dp))
     }
 }
 
