9c5724ecc57c05743d5bf9a5396353b08de51d69
dscoury <dylansc@uio.no>
2026-05-10 14:56:39 +0200

    Fikset illustrasjoner til onboarding sidene

--- app/src/main/java/no/uio/ifi/in2000/dylansc/team6project/ui/onboarding/OnboardingPages.kt
+++ app/src/main/java/no/uio/ifi/in2000/dylansc/team6project/ui/onboarding/OnboardingPages.kt
@@ -14,28 +14,33 @@ val onboardingPages = listOf(
         title = R.string.onboarding_page_press_for_data_title,
         description = R.string.onboarding_page_press_for_data_description,
         onboardingButtonLabel = R.string.onboarding_cta_next,
+        image = R.drawable.onboarding_side2,
     ),
 
     OnboardingPage(
         title = R.string.onboarding_page_search_for_location_title,
         description = R.string.onboarding_page_search_for_location_description,
-        onboardingButtonLabel = R.string.onboarding_cta_next
+        onboardingButtonLabel = R.string.onboarding_cta_next,
+        image = R.drawable.onboarding_side3,
     ),
 
     OnboardingPage(
         title = R.string.onboarding_page_clothing_tips_title,
         description = R.string.onboarding_page_clothing_tips_description,
         onboardingButtonLabel = R.string.onboarding_cta_next,
+        image = R.drawable.onboarding_side4,
     ),
 
     OnboardingPage(
         title = R.string.onboarding_page_visual_weather_title,
         description = R.string.onboarding_page_visual_weather_description,
         onboardingButtonLabel = R.string.onboarding_cta_next,
+        image = R.drawable.onboarding_side5,
     ),
     OnboardingPage(
         title = R.string.onboarding_page_danger_alerts_title,
         description = R.string.onboarding_page_danger_alerts_description,
         onboardingButtonLabel = R.string.onboarding_cta_get_started,
+        image = R.drawable.onboarding_side6,
     ),
 )
