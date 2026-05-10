a6c4bf28e7d495dd7d9f079f789565e1ff001c4a
Silje Becmann Granås <siljbg@ulrik.uio.no>
2026-05-10 09:33:18 +0200

    refaktorering av MapOsmView.kt

--- app/src/main/java/no/uio/ifi/in2000/dylansc/team6project/ui/map/components/MapOsmView.kt
+++ app/src/main/java/no/uio/ifi/in2000/dylansc/team6project/ui/map/components/MapOsmView.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.content.edit
 import no.uio.ifi.in2000.dylansc.team6project.data.warningdata.AlertFeature
 import no.uio.ifi.in2000.dylansc.team6project.data.weatherdata.AreaData
 import no.uio.ifi.in2000.dylansc.team6project.ui.map.MapScreenUiState
@@ -14,10 +15,12 @@ import no.uio.ifi.in2000.dylansc.team6project.ui.map.updateSelectedMarker
 import no.uio.ifi.in2000.dylansc.team6project.ui.map.updateUserMarker
 import no.uio.ifi.in2000.dylansc.team6project.ui.map.updateWmsLayer
 import org.osmdroid.config.Configuration
+import org.osmdroid.events.MapEventsReceiver
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory
 import org.osmdroid.util.GeoPoint
 import org.osmdroid.views.MapView
 import org.osmdroid.views.overlay.MapEventsOverlay
+import org.osmdroid.views.overlay.TilesOverlay
 
 private data class DrawnLayerState(
     val layerName: String?,
@@ -53,21 +56,22 @@ fun MapOsmView(
                         prefs.getFloat("start_lon", 10.7461f).toDouble()
                     )
                 )
-                setMinZoomLevel(3.0)
-                setMaxZoomLevel(18.0)
+                minZoomLevel = 3.0
+                maxZoomLevel = 18.0
                 setScrollableAreaLimitLatitude(85.0, -85.0, height + 1000)
-                setFlingEnabled(true)
+                isFlingEnabled = true
                 setVerticalMapRepetitionEnabled(false)
                 Configuration.getInstance().cacheMapTileCount = 5000
 
                 if (granted) centerMapOnUserLocation(ctx, this)
 
-                val mapEventsReceiver = object : org.osmdroid.events.MapEventsReceiver {
+                val mapEventsReceiver = object : MapEventsReceiver {
                     override fun singleTapConfirmedHelper(p: GeoPoint?): Boolean = false
                     override fun longPressHelper(p: GeoPoint?): Boolean {
                         p?.let {
                             updateSelectedMarker(this@apply, it)
-                        onLocationSelected(it)}
+                            onLocationSelected(it)
+                        }
                         return true
                     }
                 }
@@ -97,14 +101,14 @@ fun MapOsmView(
                     newState.area != lastState.area) {
                     if (currentLayer == null) {
                         view.overlays.removeAll(
-                            view.overlays.filterIsInstance<org.osmdroid.views.overlay.TilesOverlay>()
+                            view.overlays.filterIsInstance<TilesOverlay>()
                         )
                     } else {
                         updateWmsLayer(view, uiState)
                     }
                 }
 
-                // Update weather warning only if weatherWarning or the number of warnings have changed
+                // Update dangeralert only if weatherWarning or the number of warnings have changed
                 if (newState.dangerAlert != lastState.dangerAlert ||
                     newState.alertCount != lastState.alertCount) {
                     drawAlerts(view, uiState, uiState.dangerAlert, onAlertClick)
@@ -116,10 +120,10 @@ fun MapOsmView(
             // The users marker is always updated
             val prefs = view.context.getSharedPreferences("osmdroid", Context.MODE_PRIVATE)
             val center = view.mapCenter
-            prefs.edit()
-                .putFloat("current_lat", center.latitude.toFloat())
-                .putFloat("current_lon", center.longitude.toFloat())
-                .apply()
+            prefs.edit {
+                putFloat("current_lat", center.latitude.toFloat())
+                putFloat("current_lon", center.longitude.toFloat())
+            }
 
             if (locationServicesEnabled) {
                 geoLocation?.let { updateUserMarker(view, it) }
