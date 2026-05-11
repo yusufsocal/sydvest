2c0949b2ab0d20b5267dbea697bb57aa324b6788
Silje Becmann Granås <siljbg@ulrik.uio.no>
2026-05-11 20:11:24 +0200

    ndrer log.d til engelsk

--- app/src/main/java/no/uio/ifi/in2000/dylansc/team6project/ui/map/components/MapSearchField.kt
+++ app/src/main/java/no/uio/ifi/in2000/dylansc/team6project/ui/map/components/MapSearchField.kt
@@ -89,7 +89,7 @@ fun MapSearchField(
                         onClickLabel = stringResource(R.string.close_search),
                         role = Role.Button
                     ) {
-                        android.util.Log.d("SØK", "Bakgrunn klikket! expanded=$expanded")
+                        android.util.Log.d("Search", "Background not working! expanded=$expanded")
                         expanded = false
                         keyboardController?.hide()
                     }
